370540da481b5ea30bef4d19e80c2f5af133f035
Muhammed alajam <muhammed.alajam@gmail.com>
2023-01-06 22:03:42 +0200

    unused import deleted

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:consultations/constant.dart';
 import 'package:consultations/models/api_response.dart';
 import 'package:consultations/screens/loading.dart';
-import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
